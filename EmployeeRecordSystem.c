269cec51c5f225a3fbb3608c7e5ce3796e159b23
Savli-Sutar <its.me.savli.03@gmail.com>
2026-04-19 16:03:20 +0530

    Improve memory management and input functions
    Refactor memory allocation and input handling for employees.

--- EmployeeRecordSystem.c
+++ EmployeeRecordSystem.c
@@ -8,18 +8,36 @@ struct Employee {
     float experience;
     float salary;
 };
+// Function Prototypes
+void inputDetails(struct Employee * ptr, int n);
+void sortByExperience(struct Employee * ptr, int n);
+void displayEmployees(struct Employee * ptr, int n);
+
 int main(){
-    int n, i, j;
-    struct Employee *ptr, temp;
+    int n;
+    struct Employee *ptr;
     printf("Enter number of employees: ");
     scanf("%d", &n);
     // the malloc measures the amount of employees there are (n), and allocaates that much memory only
     ptr = (struct Employee*) malloc(n * sizeof(struct Employee));
+    if(ptr == NULL){
+        return 1;
+    }
+    //Calling the function
+    inputDetails(ptr,n);
+    sortByExperience(ptr,n);
+    displayEmployees(ptr,n);
 
+     free(ptr); // Deallocates the previously allocated memory 
+    return 0; 
+}
+void inputDetails(struct Employee * ptr, int n){
+    int i;
     for(i = 0; i<n; i++){
         printf("\n Display the details of the Employee %d:\n", i+1);
         printf("ID: ");
-        scanf(" %d",&(ptr+i)-> id); //Points to the specific ID slot
+        scanf(" %d", &(ptr+i)-> id); //Points to the specific ID slot
+         while ((getchar()) != '\n');
         printf("Name: ");
         scanf(" %[^\n]s",(ptr+i)-> name); //Points to the specific Name slot
         while ((getchar()) != '\n');
@@ -33,6 +51,11 @@ int main(){
         scanf(" %f",&(ptr+i)-> salary); // Points to the specific  Salary slot
         while ((getchar()) != '\n'); 
     }
+
+}
+void sortByExperience(struct Employee * ptr, int n){
+    int i,j;
+    struct Employee temp;
     // Sprting by experience
     // using i for counter of the number of times passing through the list of employees
     // using j for comparing two consecutive memory locations in the list
@@ -47,13 +70,22 @@ int main(){
 
         }
     }
+
+}
+void displayEmployees(struct Employee * ptr, int n){
+    int i;
     // Display Sorted Employees
     printf("\n......Employees Sorted by Experience......\n");
     for(i = 0; i < n; i++){
         printf("%s | %.1f years exp | ID %05d | %.2f Rs/- | %s \n",(ptr + i)->name,(ptr + i)->experience,(ptr + i)->id,(ptr +i)->salary,(ptr + i)->designation);
 
     }
-    free(ptr); // Deallocates the previously allocated memory 
-    return 0; 
+
 }
+   
+
+    
+    
+   
+
 
